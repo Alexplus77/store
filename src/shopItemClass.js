26d4369bb13caf1633c827da5ff1e89a66ab28a6
Alexplus77 <alexplus77@mail.ru>
2021-10-01 07:18:01 +0200

    commit

--- src/shopItemClass.js
+++ src/shopItemClass.js
@@ -1,21 +1,22 @@
 import React from "react";
 import './css/main.css'
 
-const ShopItemClass=({item})=>{
+const ShopItemClass = ({item}) => {
+    const {brand, title, description, descriptionFull, price, currency} = item
     return (
         <div className="main-content">
-            <h2>{item.brand}</h2>
-            <h1>{item. title}</h1>
-            <h3>{item. description}</h3>
+            <h2>{brand}</h2>
+            <h1>{title}</h1>
+            <h3>{description}</h3>
             <div className="description">
-                {item.descriptionFull}
+                {descriptionFull}
             </div>
             <div className="highlight-window mobile">
                 <div className="highlight-overlay"/>
             </div>
             <div className="divider"/>
             <div className="purchase-info">
-                <div className="price">{item.currency}{item. price}</div>
+                <div className="price">{currency}{price}</div>
                 <button>Добавить в корзину</button>
             </div>
         </div>
